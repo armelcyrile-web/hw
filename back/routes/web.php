50d3368307e1223359c4d3bce960129261faa258
Armel <yakpabio66@gmail.com>
2026-07-28 13:49:45 +0200

    replace render2

--- back/routes/web.php
+++ back/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+
+
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/cron/run-schedule', function () {
+    Artisan::call('schedule:run');
+
+    return response()->json([
+        'status' => 'ok',
+        'time'   => now()->toIso8601String(),
+    ]);
 });
